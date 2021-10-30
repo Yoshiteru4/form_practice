b5b505203cf9fa7b2244393bfe9e141c3fb10bc4
Yoshiteru4 <93124375+Yoshiteru4@users.noreply.github.com>
2021-10-30 23:15:50 +0900

    form

---
+++ check.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $content = $_POST['content'];
+
+   //  var_dump($name . $pass);
+
+?>
--- index.php
+++ index.php
@@ -7,6 +7,24 @@
    <title>Document</title>
 </head>
 <body>
-
+   <form action="check.php" method="POST">
+      <div>
+         名前<br>
+         <input type="text" name="name">
+      </div>
+      <div>
+         メールアドレス<br>
+         <input type="text" name="email">
+      </div>
+      <div>
+         パスワード <br>
+         <input type="password" name="pass" id="">
+      </div>
+      <div>
+         お問い合わせ内容<br>
+         <textarea name="content" id="" cols="30" rows="10"></textarea>
+      </div>
+      <input type="submit" value="送信">
+   </form>
 </body>
 </html>
